bb08cfbfaccd92ec96bb7a07f5535fa365908a55
Arpit <arpit1995gupta@gmail.com>
2023-07-17 03:01:27 +0530

    Day 1 and 2 of JS

---
+++ part-1/2.js
@@ -0,0 +1,25 @@
+// "use strict"
+
+// console.log("02")
+
+// intro to variable
+
+// variable can store some information
+// we can use that information later
+// we can change/alter that information.
+
+//declare a variable
+
+var firstName = "Arpit"
+
+
+//use a variable
+console.log("Use varaible ", firstName)
+
+
+//change value
+firstName = "Kamna"
+
+console.log("Use varaible 2nd", firstName)
+
+// we can use 'var', 'let' and 'const' to declare variable.
